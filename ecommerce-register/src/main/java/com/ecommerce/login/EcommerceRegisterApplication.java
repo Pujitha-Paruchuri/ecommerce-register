17c11f141136745f320c56472999353e7799f760
saipr <saipr@Sai-C940.attlocal.net>
2024-07-09 14:47:27 -0400

    Created master branch.

---
+++ ecommerce-register/src/main/java/com/ecommerce/login/EcommerceRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceRegisterApplication.class, args);
+	}
+
+}
