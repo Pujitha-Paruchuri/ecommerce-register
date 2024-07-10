e525e0f77d08aebbc6027c3d9d9e823293dab970
Pujitha-Paruchuri <paruchuri.puji@gmail.com>
2024-07-10 17:55:16 -0400

    Code changes for register controller.

--- ecommerce-register/src/main/java/com/ecommerce/register/controller/RegisterController.java
+++ ecommerce-register/src/main/java/com/ecommerce/register/controller/RegisterController.java
@@ -1,13 +1,28 @@
 package com.ecommerce.register.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ecommerce.register.service.impl.RegisterService;
 
 @Controller
 @RequestMapping("/register")
 public class RegisterController {
 	
-	@Post()
-	public 
+	@Autowired
+	private RegisterService registerService;
+	
+	@PostMapping(path="/account", consumes= MediaType.APPLICATION_JSON_VALUE, 
+			produces= MediaType.APPLICATION_JSON_VALUE)
+	public String registerAccount() {
+		registerAccount = registerService.
+		return null;
+	
+		
+		
+	}
 	
 }
--- ecommerce-register/src/main/java/com/ecommerce/register/service/impl/RegisterService.java
+++ ecommerce-register/src/main/java/com/ecommerce/register/service/impl/RegisterService.java
@@ -1,5 +1,9 @@
 package com.ecommerce.register.service.impl;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegisterService {
 
+	
 }
